d2ed49ca1657b06d997f587d861377d41c0611c3
ghazal fallahpour <83470392+ghazalfa@users.noreply.github.com>
2024-01-29 18:08:16 -0800

    comments ptests_and_ttests.py

--- ptests_and_ttests.py
+++ ptests_and_ttests.py
@@ -1,189 +1,88 @@
-#P and T test for All Lipids
-
 from scipy.stats import ttest_ind
 from openpyxl import Workbook
 import pandas as pd
 
-wb1 = Workbook()
+# Constants for column indices
+SEWF, EEWF = 4, 8
+SWBF, EWBF = 12, 16
+SEBF, EEBF = 20, 24
 
-SEWF = 4
-EEWF = 8
+# Dictionaries to store calculated values
+avgFoldEWF, tTestEWF, pTestEWF = {}, {}, {}
+avgFoldWBF, tTestWBF, pTestWBF = {}, {}, {}
 
-SWBF = 12
-EWBF = 16
-
-SEBF = 20
-EEBF = 24
-
-
-avgFoldEWF = {}
-tTestEWF = {}
-pTestEWF = {}
-
-avgFoldWBF = {}
-tTestWBF = {}
-pTestWBF = {}
-
-
+# List to store metabolites with invalid fold changes
 invalidFoldChange = []
 
+# Loop through metabolites in the 'total' dictionary
 for metaName in total:
     
     temp = total.get(metaName)
     
-    EWF = temp[SEWF:EEWF]
-    WBF = temp[SWBF:EWBF]
-    EBF = temp[SEBF:EEBF]
+    # Extract sublists for EWF, WBF, and EBF
+    EWF, WBF, EBF = temp[SEWF:EEWF], temp[SWBF:EWBF], temp[SEBF:EEBF]
     
-    avgEWF = sum(EWF)/len(EWF)
-    avgEBF = sum(EBF)/len(EBF)
+    # Calculate average fold change for EWF and EBF
+    avgEWF, avgEBF = sum(EWF) / len(EWF), sum(EBF) / len(EBF)
     
-    if avgEBF>0:
-        avg=avgEWF/avgEBF
+    # Calculate fold change and handle division by zero case
+    if avgEBF > 0:
+        avg_fold = avgEWF / avgEBF
     else:
         invalidFoldChange.append(metaName)
     
-
+    # Perform t-test for EWF vs EBF
     t_statistic, p_value = ttest_ind(EWF, EBF)
     
-    tTestEWF.update({metaName:t_statistic})
-    pTestEWF.update({metaName:p_value})
-    avgFoldEWF.update({metaName:avg})
+    # Update dictionaries with calculated values
+    tTestEWF.update({metaName: t_statistic})
+    pTestEWF.update({metaName: p_value})
+    avgFoldEWF.update({metaName: avg_fold})
     
-
-    
-    
+    # Perform t-test for WBF vs EBF
     t_statistic, p_value = ttest_ind(WBF, EBF)
     
-    avgWBF = sum(WBF)/len(WBF)
-    if avgEBF>0:
-        avg=avgWBF/avgEBF
+    # Calculate average fold change for WBF and handle division by zero case
+    avgWBF = sum(WBF) / len(WBF)
+    if avgEBF > 0:
+        avg_fold = avgWBF / avgEBF
     else:
         invalidFoldChange.append(metaName)
     
-    tTestWBF.update({metaName:t_statistic})
-    pTestWBF.update({metaName:p_value})
-    avgFoldWBF.update({metaName:avg})
-    
-    
-   
-        
+    # Update dictionaries with calculated values for WBF
+    tTestWBF.update({metaName: t_statistic})
+    pTestWBF.update({metaName: p_value})
+    avgFoldWBF.update({metaName: avg_fold})
 
+# Convert dictionaries to DataFrames
+df_tTestEWF = pd.DataFrame(list(tTestEWF.items()), columns=['MetaName', 'Value'])
+df_pTestEWF = pd.DataFrame(list(pTestEWF.items()), columns=['MetaName', 'Value'])
+df_avgFoldEWF = pd.DataFrame(list(avgFoldEWF.items()), columns=['MetaName', 'Value'])
 
-# Convert dictionary to DataFrame
-df = pd.DataFrame(list(tTestEWF.items()), columns=['MetaName', 'Value'])
+df_tTestWBF = pd.DataFrame(list(tTestWBF.items()), columns=['MetaName', 'Value'])
+df_pTestWBF = pd.DataFrame(list(pTestWBF.items()), columns=['MetaName', 'Value'])
+df_avgFoldWBF = pd.DataFrame(list(avgFoldWBF.items()), columns=['MetaName', 'Value'])
 
-# Write DataFrame to Excel
-writer = pd.ExcelWriter('totalTTestEBF.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
+# Function to save DataFrame to Excel
+def save_to_excel(df, filename):
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
 
-# Define worksheet and cell formats
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-cell_format = workbook.add_format({'align': 'left'})
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    cell_format = workbook.add_format({'align': 'left'})
 
-# Set column widths
-worksheet.set_column('A:A', 30, cell_format)
-worksheet.set_column('B:B', 20, cell_format)
+    worksheet.set_column('A:A', 30, cell_format)
+    worksheet.set_column('B:B', 20, cell_format)
 
-# Save and close Excel file
-writer.save()
+    writer.save()
 
-# Convert dictionary to DataFrame
-df = pd.DataFrame(list(pTestEWF.items()), columns=['MetaName', 'Value'])
+# Save DataFrames to Excel files
+save_to_excel(df_tTestEWF, 'totalTTestEBF.xlsx')
+save_to_excel(df_pTestEWF, 'totalPTestEBF.xlsx')
+save_to_excel(df_avgFoldEWF, 'totalAvgFoldEBF.xlsx')
 
-# Write DataFrame to Excel
-writer = pd.ExcelWriter('totalPTestEBF.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
+save_to_excel(df_tTestWBF, 'totalTTestWBF.xlsx')
+save_to_excel(df_pTestWBF, 'totalPTestWBF.xlsx')
+save_to_excel(df_avgFoldWBF, 'totalAvgFoldWBF.xlsx')
 
-# Define worksheet and cell formats
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-cell_format = workbook.add_format({'align': 'left'})
-
-# Set column widths
-worksheet.set_column('A:A', 30, cell_format)
-worksheet.set_column('B:B', 20, cell_format)
-
-# Save and close Excel file
-writer.save()
-
-
-# Convert dictionary to DataFrame
-df = pd.DataFrame(list(avgFoldEWF.items()), columns=['MetaName', 'Value'])
-
-# Write DataFrame to Excel
-writer = pd.ExcelWriter('totalAvgFoldEBF.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-# Define worksheet and cell formats
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-cell_format = workbook.add_format({'align': 'left'})
-
-# Set column widths
-worksheet.set_column('A:A', 30, cell_format)
-worksheet.set_column('B:B', 20, cell_format)
-
-# Save and close Excel file
-writer.save()
-
-
-
-# Convert dictionary to DataFrame
-df = pd.DataFrame(list(tTestWBF.items()), columns=['MetaName', 'Value'])
-
-# Write DataFrame to Excel
-writer = pd.ExcelWriter('totalTTestWBF.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-# Define worksheet and cell formats
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-cell_format = workbook.add_format({'align': 'left'})
-
-# Set column widths
-worksheet.set_column('A:A', 30, cell_format)
-worksheet.set_column('B:B', 20, cell_format)
-
-# Save and close Excel file
-writer.save()
-
-# Convert dictionary to DataFrame
-df = pd.DataFrame(list(pTestWBF.items()), columns=['MetaName', 'Value'])
-
-# Write DataFrame to Excel
-writer = pd.ExcelWriter('totalPTestWBF.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-# Define worksheet and cell formats
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-cell_format = workbook.add_format({'align': 'left'})
-
-# Set column widths
-worksheet.set_column('A:A', 30, cell_format)
-worksheet.set_column('B:B', 20, cell_format)
-
-# Save and close Excel file
-writer.save()
-
-
-# Convert dictionary to DataFrame
-df = pd.DataFrame(list(avgFoldWBF.items()), columns=['MetaName', 'Value'])
-
-# Write DataFrame to Excel
-writer = pd.ExcelWriter('totalAvgFoldWBF.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-# Define worksheet and cell formats
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
-cell_format = workbook.add_format({'align': 'left'})
-
-# Set column widths
-worksheet.set_column('A:A', 30, cell_format)
-worksheet.set_column('B:B', 20, cell_format)
-
-# Save and close Excel file
-writer.save()
